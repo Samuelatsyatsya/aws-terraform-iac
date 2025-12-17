c3f1b0da0092777d0605deb13cef645ad30771a1
Samuel Atsyatsya <atsyatsya1@gmail.com>
2025-12-17 13:46:22 +0000

    change instance type to t3

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this" {
   ami                    = var.ami_id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
