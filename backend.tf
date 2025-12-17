976bdeefbccb02587f159398806f727ccb5c2112
Samuel Atsyatsya <atsyatsya1@gmail.com>
2025-12-17 13:45:10 +0000

    change region to eu-central-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-12345"
     key            = "terraform/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-central-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
