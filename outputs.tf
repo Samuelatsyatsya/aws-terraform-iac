f363fa86f21546d8594ba0065287f76d119d3a52
Samuel Atsyatsya <atsyatsya1@gmail.com>
2026-01-27 13:46:33 +0000

    refactor(terraform): add root files

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,25 @@
-output "ec2_public_ip" {
-  value = module.compute.public_ip
-}
-
+# Terraform outputs for AWS infrastructure deployment
 output "vpc_id" {
-  value = module.network.vpc_id
+  description = "ID of the VPC"
+  value       = module.networking.vpc_id
 }
 
-output "storage_bucket" {
-  value = module.storage.bucket_name
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = module.networking.public_subnet_id
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = module.security_group.sg_id
+}
+
+output "ec2_instance_id" {
+  description = "EC2 instance ID"
+  value       = module.compute.instance_id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = module.compute.public_ip
 }
