4c7f7568cc3e4e91917b12f2d492ee3113eae1b2
Samuel Atsyatsya <atsyatsya1@gmail.com>
2026-01-27 12:48:24 +0000

    refactor(terraform): add networking module

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,28 +1,45 @@
+# VPC
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "network-vpc" }
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(var.tags, { Name = "${var.project}-VPC" })
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, { Name = "${var.project}-IGW" })
+}
+
+# Public Subnet
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = merge(var.tags, { Name = "${var.project}-public-subnet" })
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.this.id
-}
-
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+  tags = merge(var.tags, { Name = "${var.project}-public-rt" })
 }
 
-resource "aws_route_table_association" "assoc" {
+# Default Route to Internet Gateway
+resource "aws_route" "public_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = var.destination_cidr_block
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+# Route Table Association
+resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
